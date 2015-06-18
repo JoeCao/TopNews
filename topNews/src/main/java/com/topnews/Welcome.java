cb60ddda8f45a0a314c2e1a38cd365fabf451595
曹祖鹏 <chinalibra@163.com>
2015-06-18 19:25:39 +0800

    comment, format

--- topNews/src/main/java/com/topnews/Welcome.java
+++ topNews/src/main/java/com/topnews/Welcome.java
@@ -9,49 +9,52 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 
 public class Welcome extends Activity {
-	private AlphaAnimation start_anima;
-	View view;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		view = View.inflate(this, R.layout.welcome, null);
-		setContentView(view);
-		initView();
-		initData();
-	}
-	private void initData() {
-		start_anima = new AlphaAnimation(0.3f, 1.0f);
-		start_anima.setDuration(2000);
-		view.startAnimation(start_anima);
-		start_anima.setAnimationListener(new AnimationListener() {
-			
-			@Override
-			public void onAnimationStart(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				// TODO Auto-generated method stub
-				redirectTo();
-			}
-		});
-	}
-	
-	private void initView() {
-		
-	}
+    private AlphaAnimation start_anima;
+    View view;
 
-	private void redirectTo() {
-		startActivity(new Intent(getApplicationContext(), MainActivity.class));
-		finish();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        view = View.inflate(this, R.layout.welcome, null);
+        setContentView(view);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        //显示动画2秒
+        start_anima = new AlphaAnimation(0.3f, 1.0f);
+        start_anima.setDuration(2000);
+        view.startAnimation(start_anima);
+        start_anima.setAnimationListener(new AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // TODO Auto-generated method stub
+                redirectTo();
+            }
+        });
+    }
+
+    private void initView() {
+
+    }
+
+    private void redirectTo() {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
+    }
 }
