dfba951e1aab8cdc5ba919a331d49406f3a0bb0c
曹祖鹏 <chinalibra@163.com>
2015-06-25 09:32:04 +0800

    reformat

--- topNews/src/main/java/com/topnews/dao/ChannelDaoInface.java
+++ topNews/src/main/java/com/topnews/dao/ChannelDaoInface.java
@@ -8,18 +8,18 @@ import com.topnews.bean.ChannelItem;
 import android.content.ContentValues;
 
 public interface ChannelDaoInface {
-	public boolean addCache(ChannelItem item);
+    public boolean addCache(ChannelItem item);
 
-	public boolean deleteCache(String whereClause, String[] whereArgs);
+    public boolean deleteCache(String whereClause, String[] whereArgs);
 
-	public boolean updateCache(ContentValues values, String whereClause,
-			String[] whereArgs);
+    public boolean updateCache(ContentValues values, String whereClause,
+                               String[] whereArgs);
 
-	public Map<String, String> viewCache(String selection,
-			String[] selectionArgs);
+    public Map<String, String> viewCache(String selection,
+                                         String[] selectionArgs);
 
-	public List<Map<String, String>> listCache(String selection,
-			String[] selectionArgs);
+    public List<Map<String, String>> listCache(String selection,
+                                               String[] selectionArgs);
 
-	public void clearFeedTable();
+    public void clearFeedTable();
 }
